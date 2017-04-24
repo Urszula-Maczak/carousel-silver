9775c2c912a51d45108da06f12349d3c29f7939b
ursula <ulma83@poczta.fm>
2017-04-24 23:47:35 +0200

    works buttons next&prev with images

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(function() {
 
 			function changeSlide() {
 				//$('#next').click(function() {
-					carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+					//carouselList.animate({'marginLeft':-600}, 500, moveFirstSlide);
 				//});
 			};
 
@@ -17,5 +17,21 @@ $(function() {
 
 			};
 
-		
+			$("#next").on('click', function() {
+				//console.log("Działa przód!");
+				$("img").animate({
+					'marginLeft': '-=600px'
+				}, 500, moveFirstSlide);/*, function() {
+					$("img").find('li:last').after($(this).find('li:first'));
+					$("img").css({marginLeft:0}); //----- http://www.my-html-codes.com/easy-jquery-carousel
+				})*/
+			});
+
+			$("#prev").on('click', function() {
+				//console.log("Działa wstecz!");
+				$("img").animate({
+					'marginLeft': '+=600px'
+				}, 500, moveFirstSlide);
+			});
 });
+
